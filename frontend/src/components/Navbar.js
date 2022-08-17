4a1b47d6736e92722b7e389996dad272037f9c7b
Benny <73943596+Benny902@users.noreply.github.com>
2022-08-17 13:56:11 +0300

    n

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
-          <h1>Workout Buddy</h1>
+          <h1>Workout Buddy - By Ruslan & Benny</h1>
         </Link>
       </div>
     </header>
